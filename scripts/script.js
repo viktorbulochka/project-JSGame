be132c716a27b0e1da3b69b7d55a0729512f4dd5
vityapk <https://github.com/viktorbulochka>
2021-08-29 21:39:30 +0300

    In progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,13 @@
+/////////////////// variables ////////////////////////////
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
-
 
 let GOSound = new Audio("sounds/death.mp3");
 let jumpSound = new Audio("sounds/jump.mp3");
 let gameSound = new Audio("sounds/song.mp3");
 
-
 const card = document.getElementById("card");
 const cardScore = document.getElementById("card-score");
-
 
 let player = null;
 let score = 0;
@@ -18,9 +16,8 @@ let arrayBlocks = [];
 let enemySpeed = 5;
 let canScore = true;
 let presetTime = 1000;
-
-
-Dog = {
+let animationId = null;
+let Dog = {
     imgDog: new Image(),
     frameHeight: 0,
     frameIndex: 0,
@@ -28,18 +25,18 @@ Dog = {
     frameCount:10,
     frameRate:20,
 }
+////////////////////////////////////////////////////////////
 
+////////////////////// player //////////////////////////////
 class Player {
     constructor(x, y, size) {
         this.x = x;
         this.y = y;
         this.size = size;
         this.jumpHeight = 12;
-        //These 3 are used for jump configuration
         this.shouldJump = false;
         this.jumpCounter = 0;
         this.jumpUp = true;
-
     }
 
     draw() {
@@ -55,19 +52,16 @@ class Player {
             Dog.frameHeight = 0;
             Dog.count = 0;
         }
-
         currFrameX = 82 * (Dog.frameIndex % Dog.frameCount);
         Dog.imgDog.src = 'pictures/dog.png';
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(Dog.imgDog, currFrameX, Dog.frameHeight, 82, 61, this.x,this.y,this.size,this.size);
-
     }
 
     jump() {
         if (this.shouldJump) {
             this.jumpCounter++;
             if (this.jumpCounter < 15) {
-                //Go up
                 this.y -= this.jumpHeight;
             } else if (this.jumpCounter > 14 && this.jumpCounter < 19) {
                 this.y += 0;
@@ -86,9 +80,29 @@ setInterval(function () {
     Dog.count++;
 }, 1000 / Dog.frameRate);
 
+addEventListener("keydown", e => {
+    if (e.code === 'Space') {
+        if (!player.shouldJump) {
+            jumpSound.play();
+            player.jumpCounter = 0;
+            player.shouldJump = true;
+            canScore = true;
+        }
+    }
+});
 
+let shouldIncreaseSpeed = () => {
+    if (scoreIncrement + 10 === score) {
+        scoreIncrement = score;
+        enemySpeed++;
+        presetTime >= 100 ? presetTime -= 100 : presetTime = presetTime / 2;
+        arrayBlocks.forEach(block => {
+            block.slideSpeed = enemySpeed;
+        });
+    }
+}
 
-function startGame() {
+let startGame = () => {
     player = new Player(100, 440, 80,);
     arrayBlocks = [];
     score = 0;
@@ -98,29 +112,30 @@ function startGame() {
     presetTime = 1000;
 }
 
+let drawScore = () => {
+    let scoreString = score.toString();
+    let sc = document.getElementById('scoreTable')
+    sc.innerText = "Очки : " + scoreString;
+}
+//////////////////////////////////////////////////////////
 
-function getRandomNumber(min, max) {
+////////////////// entities //////////////////////////////
+let getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//Returns true of colliding
-function squaresColliding(player, block) {
+let squaresColliding = (player, block) => {
     let s1 = Object.assign(Object.create(Object.getPrototypeOf(player)), player);
     let s2 = Object.assign(Object.create(Object.getPrototypeOf(block)), block);
-    //Don't need pixel perfect collision detection
     s2.size = s2.size - 10;
     s2.x = s2.x + 10;
     s2.y = s2.y + 10;
     return !(
-        s1.x > s2.x + s2.size || //R1 is to the right of R2
-        s1.x + s1.size < s2.x || //R1 to the left of R2
-        s1.y > s2.y + s2.size || //R1 is below R2
-        s1.y + s1.size < s2.y //R1 is above R2
+        s1.x > s2.x + s2.size || s1.x + s1.size < s2.x || s1.y > s2.y + s2.size || s1.y + s1.size < s2.y
     )
 }
 
-
-function isPastBlock(player, block) {
+let isPastBlock = (player, block) => {
     return (
         player.x + (player.size / 2) > block.x + (block.size / 4) &&
         player.x + (player.size / 2) < block.x + (block.size / 4) * 3
@@ -145,19 +160,15 @@ class AvoidBlock {
         this.draw();
         this.x -= this.slideSpeed;
     }
-
 }
 
-
-function generateBlocks() {
+let generateBlocks = () => {
     let timeDelay = randomInterval(presetTime);
     arrayBlocks.push(new AvoidBlock(55, enemySpeed));
-
-
     setTimeout(generateBlocks, timeDelay);
 }
 
-function randomInterval(timeInterval) {
+let randomInterval = (timeInterval) => {
     let returnTime = timeInterval;
     if (Math.random() < 0.9) {
         returnTime += getRandomNumber(presetTime / 3, presetTime * 1.5);
@@ -166,36 +177,14 @@ function randomInterval(timeInterval) {
     }
     return returnTime;
 }
+/////////////////////////////////////////////////////////////////////
 
-
-function drawScore() {
-    let scoreString = score.toString();
-    let sc = document.getElementById('scoreTable')
-    sc.innerText = "Очки : " + scoreString;
-}
-
-function shouldIncreaseSpeed() {
-    if (scoreIncrement + 10 === score) {
-        scoreIncrement = score;
-        enemySpeed++;
-        presetTime >= 100 ? presetTime -= 100 : presetTime = presetTime / 2;
-        arrayBlocks.forEach(block => {
-            block.slideSpeed = enemySpeed;
-        });
-        console.log("Speed increased");
-    }
-}
-
-
-let animationId = null;
-
-function animate() {
+///////////////////////////// game //////////////////////////////////
+let animate = () => {
     animationId = requestAnimationFrame(animate);
     drawScore();
     player.draw();
-
     shouldIncreaseSpeed();
-
     arrayBlocks.forEach((arrayBlock, index) => {
         arrayBlock.slide();
         if (squaresColliding(player, arrayBlock)) {
@@ -217,23 +206,7 @@ function animate() {
             }, 0)
         }
     });
-
 }
-
-addEventListener("keydown", e => {
-    if (e.code === 'Space') {
-        if (!player.shouldJump) {
-            jumpSound.play();
-            player.jumpCounter = 0;
-            player.shouldJump = true;
-            canScore = true;
-        }
-    }
-});
-
-
-
-
 
 startGame();
 animate();
@@ -241,8 +214,7 @@ setTimeout(() => {
     generateBlocks();
 }, randomInterval(presetTime))
 
-
-function restartGame(button) {
+let restartGame = (button) => {
     Dog.count = null;
     card.style.display = "none";
     button.blur();
@@ -251,4 +223,5 @@ function restartGame(button) {
     requestAnimationFrame(animate)
     gameSound.play();
 }
+//////////////////////////////////////////////////////////////////
 
